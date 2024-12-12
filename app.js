1eefe0b13d417ea641d0a17f85c54e2569a921b9
itc-s23006 <s23006@std.it-college.ac.jp>
2024-12-12 15:47:20 +0900

    /otherにアクセスしたらother.ejsを使った表示を行うように修正。

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { title } = require('process');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const style_css = fs.readFileSync('./style.css', 'utf8');
+const other_page = fs.readFileSync('./other.ejs', 'utf8');
 
 let server = http.createServer(getFromClient);
 
@@ -16,22 +17,27 @@ console.log('Server start!');
 
 // createServerの処理
 function getFromClient(request, response) {
+
     let url_parts = url.parse(request.url);
     switch (url_parts.pathname) {
 
         case '/':
-            let content = ejs.render(index_page, {
+            var content = ejs.render(index_page, {
                 title: "Index",
-                content: "これはテンプレートを使ったサンプルページです。",
+                content: "これはIndexページです。",
             });
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(content);
             response.end();
             break;
 
-            case '/style.css':
-                response.writeHead(200, { 'Content-Type': 'text/css' });
-                response.write(style_css);
+            case '/other':
+                var content = ejs.render(other_page, {
+                    title: "Other",
+                    content: "これは新しく用意したページです。"
+                });
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(content);
                 response.end();
                 break;
 
@@ -39,6 +45,5 @@ function getFromClient(request, response) {
                     response.writeHead(200, { 'Content-Type': 'text/plain' });
                     response.end('no page...');
                     break;
-                    
     }
 }
