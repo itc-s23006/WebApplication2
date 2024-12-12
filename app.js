b882ac6100230867a25485b713ba112b5892a615
itc-s23006 <s23006@std.it-college.ac.jp>
2024-12-12 15:07:19 +0900

    スタイルシートの読み込み処理を追加。

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
+const url = require('url');
 const { title } = require('process');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
 
 let server = http.createServer(getFromClient);
 
@@ -14,11 +16,29 @@ console.log('Server start!');
 
 // createServerの処理
 function getFromClient(request, response) {
-    let content = ejs.render(index_page, {
-        title: "Indexページ",
-        content: "これはテンプレートを使ったサンプルページです。"
-    });
+    let url_parts = url.parse(request.url);
+    switch (url_parts.pathname) {
+
+        case '/':
+            let content = ejs.render(index_page, {
+                title: "Index",
+                content: "これはテンプレートを使ったサンプルページです。",
+            });
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(content);
             response.end();
+            break;
+
+            case '/style.css':
+                response.writeHead(200, { 'Content-Type': 'text/css' });
+                response.write(style_css);
+                response.end();
+                break;
+
+                default:
+                    response.writeHead(200, { 'Content-Type': 'text/plain' });
+                    response.end('no page...');
+                    break;
+                    
+    }
 }
