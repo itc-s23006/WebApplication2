1195577da544b33d53831a6fd4a93a057871f382
itc-s23006 <s23006@std.it-college.ac.jp>
2024-12-12 12:37:19 +0900

    HTMLで日本語を表示する。

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const http = require('http');
 
 let server = http.createServer(
     (request, response) => {
-        response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>');
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<!DOCTYPE html><html lang="ja"');
+        response.write('<head><meta charset="utf-8">');
+        response.write('<title>Hello</title></head>');
+        response.write('<body><h1>Hello Node.js!</h1>');
+        response.write('<p>This is Node.js sample page.</p>');
+        response.write('<p>これは、Node.jsのサンプルページです。</p>', 'utf-8');
+        response.write('</body></html>');
+        response.end();
     }
 );
 
 server.listen(3000);
+console.log('Server start!');
