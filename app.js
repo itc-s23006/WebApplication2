4714bc77de0fbf93ea44ffee31371d76f73fb1ec
itc-s23006 <s23006@std.it-college.ac.jp>
2024-12-17 16:54:21 +0900

    app.js、しっかり起動できた。フォームの処理を作成。

--- app.js
+++ app.js
@@ -8,42 +8,41 @@ const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs', 'utf8');
 const style_css = fs.readFileSync('./style.css', 'utf8');
 
-let server = http.createServer(getFormClient);
+let server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('Server start!');
 
-// ここまでメインプログラム
+// メインプログラムの処理
 
 // createServerの処理
-function getFormClient(request, response) {
-
+function getFromClient(request, response) {
     let url_parts = url.parse(request.url, true);
     switch (url_parts.pathname) {
-
         case '/':
             response_index(request, response);
             break;
-            
-            case '/other':
-                response_other(request, response);
-                break;
 
-                case '/style.css':
-                    response.writeHead(200, { 'Content-Type': 'text/css' });
-                    response.write(style_css);
-                    response.end();
-                    break;
+        case '/other':
+            response_other(request, response);
+            break;
 
-                    default:
-                        response.writeHead(200, { 'Content-Type': 'text/plain' });
-                        response.end('no page...');
-                        break;
+        case '/style.css':
+            response.writeHead(200, { 'Content-Type': 'text/css' });
+            response.write(style_css);
+            response.end();
+            break;
+
+        default:
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end('no page...');
+            break;
     }
 }
 
+// Indexページの処理
 function response_index(request, response) {
-    let msg = "これはIndexページです。"
+    let msg = "これはIndexページです。";
     let content = ejs.render(index_page, {
         title: "Index",
         content: msg
@@ -53,17 +52,20 @@ function response_index(request, response) {
     response.end();
 }
 
+// Otherページの処理
 function response_other(request, response) {
-    let msg = "これはOtherページです。"
+    let msg = "これはOtherページです。";
 
     if (request.method == 'POST') {
         let body = '';
 
+        // POSTデータを受信
         request.on('data', (data) => {
             body += data;
         });
 
-        response.on('end', () => {
+        // データ受信完了後に処理
+        request.on('end', () => {
             let post_data = qs.parse(body);
             msg += 'あなたは、「' + post_data.msg + '」と書きました。';
             let content = ejs.render(other_page, {
@@ -73,16 +75,15 @@ function response_other(request, response) {
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(content);
             response.end();
-        }); 
-        
-        } else {
-            let msg = "ページがありません。"
-            let content = ejs.render(other_page, {
-                title: "Other",
-                content: msg
-            });
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(content);
-            response.end();
+        });
+
+    } else {
+        let content = ejs.render(other_page, {
+            title: "Other",
+            content: msg
+        });
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(content);
+        response.end();
     }
 }
