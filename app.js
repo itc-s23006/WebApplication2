2274f48413669ce3321f233e6aa2aeaa78b0ad05
itc-s23006 <s23006@std.it-college.ac.jp>
2024-12-19 15:54:23 +0900

    /mydataでアクセスするとmydata.txtに保存されてる中身を表示。

--- app.js
+++ app.js
@@ -27,8 +27,12 @@ function getFromClient(request, response) {
             response_index(request, response);
             break;
 
-        case '/login':// ログインページ
+        case '/login': // ログインページ
             response_login(request, response);
+            break;
+
+        case '/mydata': // mydata.txt をプレーンテキストで表示
+            response_mydata(request, response);
             break;
 
         default:
@@ -41,70 +45,83 @@ function getFromClient(request, response) {
 // loginのアクセス処理
 function response_login(request, response) {
     let content = ejs.render(login_page, {});
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(content);
-            response.end();
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(content);
+    response.end();
+}
+
+// indexのアクセス処理
+function response_index(request, response) {
+    if (request.method == 'POST') {
+        let body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            let data = qs.parse(body);
+            addToData(data.id, data.msg, filename, request);
+            write_index(request, response);
+        });
+    } else {
+        write_index(request, response);
+    }
+}
+
+// /mydata のアクセス処理
+function response_mydata(request, response) {
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+            response.writeHead(500, { 'Content-Type': 'text/plain' });
+            response.end('Error reading file.');
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end(data); // ファイルの中身をそのまま返す
         }
+    });
+}
 
-        // indexのアクセス処理
-        function response_index(request, response) {
-            // POSTアクセス時の処理
-            if (request.method == 'POST') {
-                let body = '';
+// indexのページ作成
+function write_index(request, response) {
+    let msg = "※何かメッセージを書いてください。";
+    let content = ejs.render(index_page, {
+        title: 'Index',
+        content: msg,
+        data: message_data,
+        filename: 'data_item'
+    });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(content);
+    response.end();
+}
 
-                // データ受信のイベント処理
-                request.on('data', function (data) {
-                    body += data;
-                });
-
-                // データ受信終了のイベント処理
-                request.on('end', function() {
-                    data = qs.parse(body);
-                    addToData(data.id, data.msg, filename, request);
-                    write_index(request, response);
-                });
-            } else {
-                write_index(request, response);
-            }
+// テキストファイルをダウンロード
+function readFromFile(fname) {
+    fs.readFile(fname, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Error reading file:', err);
+            message_data = [];
+        } else {
+            message_data = data.split('\n').filter(line => line !== '');
         }
+    });
+}
 
-        // indexのページ作成
-        function write_index(request, response) {
-            let msg = "※何かメッセージを書いてください。";
-            let content = ejs.render(index_page, {
-                title: 'Index',
-                content: msg,
-                data: message_data,
-                filename: 'data_item'
-            });
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(content);
-            response.end();
-        }
+// データを更新
+function addToData(id, msg, fname, request) {
+    let obj = { 'id': id, 'msg': msg };
+    let obj_str = JSON.stringify(obj);
+    console.log('add data: ' + obj_str);
+    message_data.unshift(obj_str);
+    if (message_data.length > max_num) {
+        message_data.pop();
+    }
+    saveToFile(fname);
+}
 
-        // テキストファイルをダウンロード
-        function readFromFile(fname) {
-            fs.readFile(fname, 'utf8', (err, data) => {
-                message_data = data.split('\n');
-            })
-        }
-
-        // データを更新
-        function addToData(id, msg, fname, request) {
-            let obj = { 'id': id, 'msg': msg };
-            let obj_str = JSON.stringify(obj);
-            console.log('add data: ' + obj_str);
-            message_data.unshift(obj_str);
-            if (message_data.length > max_num) {
-                message_data.pop();
-            }
-            saveToFile(fname);
-        }
-
-        // データを保存
-        function saveToFile(fname) {
-            let data_str = message_data.join('\n');
-            fs.writeFile(fname, data_str, (err) => {
-                if (err) { throw err; }
-            });
-        }
+// データを保存
+function saveToFile(fname) {
+    let data_str = message_data.join('\n');
+    fs.writeFile(fname, data_str, (err) => {
+        if (err) { throw err; }
+    });
+}
