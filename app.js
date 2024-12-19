35bc9b559ce7bf2909bb3c87618884b9647d43fa
itc-s23006 <s23006@std.it-college.ac.jp>
2024-12-19 10:05:05 +0900

    メインプログラムを作成。

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-let data = {
-    'Taro': '09-999-999',
-    'Hanako': '080-888-888',
-    'Sachiko': '070-777-777',
-    'Ichiro': '060-666-666'
-}
-
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
@@ -12,32 +5,30 @@ const url = require('url');
 const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
-const other_page = fs.readFileSync('./other.ejs', 'utf8');
-const style_css = fs.readFileSync('./style.css', 'utf8');
+const login_page = fs.readFileSync('./login.ejs', 'utf8');
+
+const max_num = 10; // 最大保管数
+const filename = 'mydata.txt'; // データファイル名
+let message_data; // データ
+readFromFile(filename);
 
 let server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('Server start!');
 
-// メインプログラムの処理
+// ここまでメインプログラム
 
 // createServerの処理
 function getFromClient(request, response) {
     let url_parts = url.parse(request.url, true);
     switch (url_parts.pathname) {
-        case '/':
+        case '/': // トップページ(メッセージボード)
             response_index(request, response);
             break;
 
-        case '/other':
-            response_other(request, response);
-            break;
-
-        case '/style.css':
-            response.writeHead(200, { 'Content-Type': 'text/css' });
-            response.write(style_css);
-            response.end();
+        case '/login':// ログインページ
+            response_login(request, response);
             break;
 
         default:
@@ -47,51 +38,73 @@ function getFromClient(request, response) {
     }
 }
 
-// Indexページの処理
-function response_index(request, response) {
-    let msg = "これはIndexページです。";
-    let content = ejs.render(index_page, {
-        title: "Index",
-        content: msg,
-        data: data
-    });
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(content);
-    response.end();
-}
+// loginのアクセス処理
+function response_login(request, response) {
+    let content = ejs.render(login_page, {});
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(content);
+            response.end();
+        }
 
-// Otherページの処理
-function response_other(request, response) {
-    let msg = "これはOtherページです。";
+        // indexのアクセス処理
+        function response_index(request, response) {
+            // POSTアクセス時の処理
+            if (request.method == 'POST') {
+                let body = '';
 
-    if (request.method == 'POST') {
-        let body = '';
+                // データ受信のイベント処理
+                request.on('data', function (data) {
+                    body += data;
+                });
 
-        // POSTデータを受信
-        request.on('data', (data) => {
-            body += data;
-        });
+                // データ受信終了のイベント処理
+                request.on('end', function() {
+                    data = qs.parse(body);
+                    addToData(data.id, data.msg, filename, request);
+                    write_index(request, response);
+                });
+            } else {
+                write_index(request, response);
+            }
+        }
 
-        // データ受信完了後に処理
-        request.on('end', () => {
-            let post_data = qs.parse(body);
-            msg += 'あなたは、「' + post_data.msg + '」と書きました。';
-            let content = ejs.render(other_page, {
-                title: "Other",
-                content: msg
+        // indexのページ作成
+        function write_index(request, response) {
+            let msg = "※何かメッセージを書いてください。";
+            let content = ejs.render(index_page, {
+                title: 'Index',
+                content: msg,
+                data: message_data,
+                filename: 'data_item'
             });
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(content);
             response.end();
-        });
+        }
 
-    } else {
-        let content = ejs.render(other_page, {
-            title: "Other",
-            content: msg
-        });
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(content);
-        response.end();
-    }
-}
+        // テキストファイルをダウンロード
+        function readFromFile(fname) {
+            fs.readFile(fname, 'utf8', (err, data) => {
+                message_data = data.split('\n');
+            })
+        }
+
+        // データを更新
+        function addToData(id, msg, fname, request) {
+            let obj = { 'id': id, 'msg': msg };
+            let obj_str = JSON.stringify(obj);
+            console.log('add data: ' + obj_str);
+            message_data.unshift(obj_str);
+            if (message_data.length > max_num) {
+                message_data.pop();
+            }
+            saveToFile(fname);
+        }
+
+        // データを保存
+        function saveToFile(fname) {
+            let data_str = message_data.join('\n');
+            fs.writeFile(fname, data_str, (err) => {
+                if (err) { throw err; }
+            });
+        }
