102f3b4ec3be6634fad2c432b20f5c034afb0a82
itc-s23006 <s23006@std.it-college.ac.jp>
2025-01-15 15:11:24 +0900

    クエリーパラメーターを使おう。

--- routes/hello.js
+++ routes/hello.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    let name = req.query.name;
+    let mail = req.query.mail;
     let data = {
         title: 'Hello!',
-        content: 'これは、サンプルのコンテンツです。<br>this is sample content.'
+        content: 'あなたの名前は、' + name + '。<br>' +
+        'メールアドレスは、' + mail + 'です。'
     };
     res.render('hello', data);
 });
