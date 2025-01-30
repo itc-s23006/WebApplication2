868018fa502e54347e45eb1a01dd1c54538735e3
itc-s23006 <s23006@std.it-college.ac.jp>
2025-01-30 15:35:13 +0900

    コメント追加

--- routes/hello.js
+++ routes/hello.js
@@ -1,26 +1,34 @@
-const express = require('express');
+const express = require('express'); // expressモジュールを読み込み。
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    let msg = '※何か書いて送信して下さい。';
+router.get('/', (req, res, next) => { // GETリクエストを処理するルート。 
+    let msg = '※何か書いて送信して下さい。'; 
+
     if (req.session.message != undefined) {
-        msg = "Last Message: " + req.session.message;
+        msg = "Last Message: " + req.session.message; // 最後に送信されたメッセージを表示。
     }
+
     let data = {
-        title: 'Hello!',
-        content: msg
+        title: 'Hello!', // ページのタイトル
+        content: msg     // 画面に表示するメッセージ
     };
-    res.render('hello', data);
+
+    res.render('hello', data); // 'hello'というテンプレートをレンダリングして、データを送信。
 });
 
-router.post('/post', (req, res, next) => {
-    let msg = req.body['message'];
+router.post('/post', (req, res, next) => { // POSTリクエストを処理するルート。
+
+    let msg = req.body['message']; // フォームから送信されたメッセージを取得。
+
     req.session.message = msg;
+
     let data = {
-        title: 'Hello!',
-        content: 'Last Message: ' + req.session.message
+        title: 'Hello!', // ページのタイトル
+        content: 'Last Message: ' + req.session.message // 最後に送信されたメッセージを表示
     };
-    res.render('hello', data);
+
+    res.render('hello', data); // 'hello'というテンプレートをレンダリングして、データを送信。
 });
 
-module.exports = router;
+module.exports = router; // このルーターをモジュールとしてエクスポート。
