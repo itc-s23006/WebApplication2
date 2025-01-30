bda31437812eb5519963bfc409740b322e51c9e0
itc-s23006 <s23006@std.it-college.ac.jp>
2025-01-30 14:53:29 +0900

    テキストをセッションに保存。

--- routes/hello.js
+++ routes/hello.js
@@ -2,18 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    let msg = '※何か書いて送信して下さい。';
+    if (req.session.message != undefined) {
+        msg = "Last Message: " + req.session.message;
+    }
     let data = {
         title: 'Hello!',
-        content: '何か書いて送信してください。'
+        content: msg
     };
     res.render('hello', data);
 });
 
 router.post('/post', (req, res, next) => {
     let msg = req.body['message'];
+    req.session.message = msg;
     let data = {
         title: 'Hello!',
-        content: 'あなたは、「' + msg + '」と送信しました。'
+        content: 'Last Message: ' + req.session.message
     };
     res.render('hello', data);
 });
