ac1dec73963c89da00efb26ddde868d6bfe69845
itc-s23006 <s23006@std.it-college.ac.jp>
2025-02-18 15:55:42 +0900

    データベースアクセスの処理を作る。

--- routes/hello.js
+++ routes/hello.js
@@ -1,34 +1,20 @@
 const express = require('express'); // expressモジュールを読み込み。
-
 const router = express.Router();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('mydb.db');
 
-router.get('/', (req, res, next) => { // GETリクエストを処理するルート。 
-    let msg = '※何か書いて送信して下さい。'; 
-
-    if (req.session.message != undefined) {
-        msg = "Last Message: " + req.session.message; // 最後に送信されたメッセージを表示。
-    }
-
-    let data = {
-        title: 'Hello!', // ページのタイトル
-        content: msg     // 画面に表示するメッセージ
-    };
-
-    res.render('hello', data); // 'hello'というテンプレートをレンダリングして、データを送信。
-});
-
-router.post('/post', (req, res, next) => { // POSTリクエストを処理するルート。
-
-    let msg = req.body['message']; // フォームから送信されたメッセージを取得。
-
-    req.session.message = msg;
-
-    let data = {
-        title: 'Hello!', // ページのタイトル
-        content: 'Last Message: ' + req.session.message // 最後に送信されたメッセージを表示
-    };
-
-    res.render('hello', data); // 'hello'というテンプレートをレンダリングして、データを送信。
-});
+router.get('/', (req, res, next) => {
+    db.serialize(() => {
+        db.all("select * from mydata", (err, rows) => {
+            if (!err) {
+                let data = {
+                    title: 'Hello!',
+                    content: rows
+                };
+                res.redirect('hello', data);
+            }
+        });
+    });
+ });  
 
 module.exports = router; // このルーターをモジュールとしてエクスポート。
